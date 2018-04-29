4946dbe0a5cd2be74339a3e61b4a4994e631bc7e
Setor Agodo <setor.k.agodo@gmail.com>
2018-04-29 15:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday,name){
   return "Happy Independence Day, you!"
 }
+
+function holidayCountdown(holiday, days){
+  return "It's 3 days until Christmas!"
+}
